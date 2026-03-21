b7f823a2c382dc90adfa4d0f2517580c08f78dd7
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-21 17:18:32 +0300

    Correct compiler detection for 11.x

--- clean-build.bat
+++ clean-build.bat
@@ -1,4 +1,4 @@
-@echo off
+@if "x%VERBOSE%"=="x" echo off
 rem vim:set sw=4 ts=8 et fileencoding=utf8:
 rem SPDX-License-Identifier: BSD-2-Clause
 rem SPDX-FileCopyrightText: 2025 Сергей Леонтьев (leo@sai.msu.ru)
